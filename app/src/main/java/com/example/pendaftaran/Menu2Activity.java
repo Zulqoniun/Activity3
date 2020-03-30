486e3d8eb11436a3dcebdd64c547a380f0b6d01c
Zulqoniun <muhammadqoni123@gmail.com>
2020-03-30 19:05:44 +0700

    Activity3

---
+++ app/src/main/java/com/example/pendaftaran/Menu2Activity.java
@@ -0,0 +1,16 @@
+package com.example.pendaftaran;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.activity3.R;
+
+public class Menu2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu2);
+    }
+}
